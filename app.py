2a22ad51ec0827862d9105068f4a207d18d1ca28
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-14 14:22:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,17 +20,29 @@ class InferlessPythonModel:
         self.ws = check_comfyui(self.server_address,self.client_id)
 
     def infer(self, inputs):
-        workflow_name = inputs.get("workflow_name")
+        workflow_input = inputs.get("workflow")
         prompt = inputs.get("prompt")
         negative_prompt = inputs.get("negative_prompt")
+        workflow_filename = "workflow_api.json"
+        workflow_path = os.path.join(self._data_dir, workflow_filename)
+        # Process the workflow input
+        if workflow_input.startswith('http://') or workflow_input.startswith('https://'):
+            response = requests.get(workflow_input)
+            workflow_json = response.json()
+        else:
+            workflow_json = json.loads(workflow_input)
         
-        workflow = load_workflow(self.directory_path,workflow_name)
-        prompt = prompt_update_workflow(workflow_name,workflow,prompt)
-        prompt_id = send_comfyui_request(self.ws, prompt, self.server_address,self.client_id)
-        file_path = get_img_file_path(self.server_address,prompt_id)
+        # Save the workflow JSON to a file
+        with open(workflow_path, 'w') as f:
+            json.dump(workflow_json, f)
+        
+        # Load the saved workflow
+        workflow = load_workflow(self._data_dir, workflow_filename)
+        prompt = prompt_update_workflow(workflow_filename, workflow, prompt)
+        prompt_id = send_comfyui_request(self.ws, prompt, self.server_address, self.client_id)
+        file_path = get_img_file_path(self.server_address, prompt_id)
         image_base64 = image_to_base64(file_path)
-        
-        return {"generated_image_base64":image_base64}
+        return {"generated_image_base64": image_base64}
     
     def finalize(self):
         pass
