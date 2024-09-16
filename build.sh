3f8c191df8a397e1b6db9a04fe56fd90253b524e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-16 21:19:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,48 +1,8 @@
 VOL_DIR="$NFS_VOLUME"
-COMFY_DIR="$VOL_DIR/ComfyUI"
-# Check if the ComfyUI directory exists
-if [ ! -d "$COMFY_DIR" ]; then
-    cd "$VOL_DIR"
-    git clone https://github.com/comfyanonymous/ComfyUI.git && cd ComfyUI && git pull
-    pip install -r requirements.txt
-else
-    cd "$COMFY_DIR" && git pull
-fi
-
-# Create necessary directories if they don't exist
-mkdir -p "$COMFY_DIR/models/unet"
-mkdir -p "$COMFY_DIR/models/clip"
-mkdir -p "$COMFY_DIR/models/vae"
-mkdir -p "$VOL_DIR/workflows"
-# Function to download file with progress bar if it doesn't exist
-download_file() {
-    local url=$1
-    local destination=$2
-    local header=$3
-    local filename=$(basename "$destination")
-
-    if [ ! -f "$destination" ]; then
-        if [ -n "$header" ]; then
-            wget --header="$header" --progress=bar:force -c -O "$destination" "$url"
-        else
-            wget --progress=bar:force -c -O "$destination" "$url"
-        fi
-    fi
-}
-
-# Authorization header for Hugging Face
-AUTH_HEADER="Authorization: Bearer $HF_ACCESS_TOKEN"
-
-# Download UNET model with authorization header
-download_file "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors" "$COMFY_DIR/models/unet/flux1-dev.safetensors" "$AUTH_HEADER"
-
-# Download CLIP models without authorization header
-download_file "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" "$COMFY_DIR/models/clip/t5xxl_fp16.safetensors"
-download_file "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" "$COMFY_DIR/models/clip/clip_l.safetensors"
-download_file "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors" "$COMFY_DIR/models/clip/t5xxl_fp8_e4m3fn.safetensors"
-
-# Download VAE model without authorization header
-download_file "https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors" "$COMFY_DIR/models/vae/ae.safetensors"
-
-# Download SD1.5 for workflow-2
-download_file "https://huggingface.co/autismanon/modeldump/resolve/main/dreamshaper_8.safetensors" "$COMFY_DIR/models/checkpoints/dreamshaper_8.safetensors"
+comfy --skip-prompt --workspace=$VOL_DIR/comfy-ui install --nvidia
+comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors --relative-path models/unet --set-civitai-api-token $HF_ACCESS_TOKEN
+comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors --relative-path models/clip
+comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors --relative-path models/clip
+comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn.safetensors --relative-path models/clip
+comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors --relative-path models/vae
+comfy --skip-prompt model download --url https://huggingface.co/autismanon/modeldump/resolve/main/dreamshaper_8.safetensors --relative-path models/checkpoints
