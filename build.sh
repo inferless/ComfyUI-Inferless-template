b9ba9739523ee136faa039be375611d9264268f6
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-16 21:35:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 VOL_DIR="$NFS_VOLUME"
-comfy --skip-prompt --workspace=$VOL_DIR/comfy-ui install --nvidia
+comfy --skip-prompt --workspace=$VOL_DIR/ComfyUI install --nvidia
 comfy --skip-prompt model download --url https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors --relative-path models/unet --set-civitai-api-token $HF_ACCESS_TOKEN
 comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors --relative-path models/clip
 comfy --skip-prompt model download --url https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors --relative-path models/clip
